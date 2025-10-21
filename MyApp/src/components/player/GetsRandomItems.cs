c1fe78417b8040ca8776729080960140c3404d2e
DarkenedFlames <g2minecraft@gmail.com>
2025-10-21 03:25:37 -0500

    debug: added items to debug queue.

--- MyApp/src/components/player/GetsRandomItems.cs
+++ MyApp/src/components/player/GetsRandomItems.cs
@@ -2,8 +2,31 @@ namespace CBA
 {
     public class GetsRandomItems(Entity owner) : Component(owner)
     {
-        public static bool DebugModeEnabled = false; // Toggle this on for testing
-        public static Queue<string> DebugItemQueue = new(); // Items to spawn in order
+        public bool DebugModeEnabled = true; // Toggle this on for testing
+        public Queue<string> DebugItemQueue = new(
+        [
+            "sonic_helmet",
+            "sonic_chestplate",
+            "sonic_leggings",
+            "olfactory_helmet",
+            "olfactory_chestplate",
+            "olfactory_leggings",
+            "tact_helmet",
+            "tact_chestplate",
+            "tact_leggings",
+            "relish_helmet",
+            "relish_chestplate",
+            "relish_leggings",
+            "vanity_helmet",
+            "vanity_chestplate",
+            "vanity_leggings",
+            "avarice_helmet",
+            "avarice_chestplate",
+            "avarice_leggings",
+            "temper_helmet",
+            "temper_chestplate",
+            "temper_leggings"
+        ]);
         protected override void RegisterSubscriptions()
         {
             RegisterSubscription<Action<Entity>>(
